dc2d5add7da1de3c59be871dfc605ac55c8d68de
Hadam Cho <chohadam49@gmail.com>
2022-09-28 17:48:56 +0900

    카카오 로그인 버튼 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,15 @@
+import Layout from "../components/Layout";
+import kakaoLoginButton from "../assets/kakao_login_large_wide.png";
+
 const Login = () => {
-  return <div>login</div>;
+  return (
+    <Layout>
+      <h1 className="font-bold text-3xl">Login</h1>
+      <button>
+        <img src={kakaoLoginButton} alt="Login with Kakao" />
+      </button>
+    </Layout>
+  );
 };
 
 export default Login;
