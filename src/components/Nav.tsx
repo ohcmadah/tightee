decb111bc002eaa446aa20aa57dd0d6d6deaceff
Hadam Cho <chohadam49@gmail.com>
2022-12-05 11:44:24 +0900

    fix: 하위 페이지일 때 메뉴 선택 안 되어 있는 문제 수정

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import cn from "classnames";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useMatch } from "react-router-dom";
 
 import homeIcon from "../assets/home.png";
 import questionIcon from "../assets/question.png";
@@ -13,31 +13,32 @@ const pages = [
   { path: "/profile", icon: eyesIcon },
 ];
 
-const Nav = () => {
-  const { pathname } = useLocation();
-
+const Menu = ({ path, icon }: { path: string; icon: string }) => {
+  const isMatched = useMatch(path + "/*");
   return (
-    <nav>
-      <div className="h-nav" />
-      <ul className="fixed bottom-0 left-0 z-nav flex h-nav w-full items-center gap-x-5 bg-white bg-primary-peach px-12 py-2">
-        {pages.map(({ path, icon }) => (
-          <li key={path} className="h-full w-full">
-            <Link
-              to={path}
-              className={cn(
-                "inline-flex h-full w-full items-center justify-center rounded-lg",
-                {
-                  "bg-primary": path === pathname,
-                }
-              )}
-            >
-              <img width={30} src={icon} alt={path} />
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <li key={path} className="h-full w-full">
+      <Link
+        to={path}
+        className={cn(
+          "inline-flex h-full w-full items-center justify-center rounded-lg",
+          { "bg-primary": isMatched }
+        )}
+      >
+        <img width={30} src={icon} alt={path} />
+      </Link>
+    </li>
   );
 };
+
+const Nav = () => (
+  <nav>
+    <div className="h-nav" />
+    <ul className="fixed bottom-0 left-0 z-nav flex h-nav w-full items-center gap-x-5 bg-white bg-primary-peach px-12 py-2">
+      {pages.map(({ path, icon }) => (
+        <Menu key={path} path={path} icon={icon} />
+      ))}
+    </ul>
+  </nav>
+);
 
 export default Nav;
