1b3fc848f85c79f6018e6154e40f60cc89c4b04a
Hadam Cho <52340070+ohcmadah@users.noreply.github.com>
2022-12-19 00:37:11 +0900

    fix: MBTI 순위 3등까지만 표시 (#48)
    * fix: MBTI 순위 3등까지만 보이도록 변경
    
    * chore: MBTI 순위 → MBTI 단합 랭킹

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -285,7 +285,7 @@ const MBTIRankReport = () => {
   if (answer.user.MBTI === null) {
     return (
       <Box>
-        <Title icon={rankIcon}>MBTI 순위</Title>
+        <Title icon={rankIcon}>MBTI 단합 랭킹</Title>
         <EmptyMBTI />
       </Box>
     );
@@ -296,13 +296,16 @@ const MBTIRankReport = () => {
 
   return (
     <Box>
-      <Title icon={rankIcon}>MBTI 순위</Title>
+      <Title icon={rankIcon}>MBTI 단합 랭킹</Title>
       <Chart.Summary value={myRank + "등"}>
         {"16개 MBTI 중에서 {value}으로 대답이 일치해요."}
       </Chart.Summary>
       <div className="mt-5 last:mb-0">
-        {rank.map(({ mbti, option, ratio }, index) => (
-          <div key={mbti} className="mb-3">
+        {rank.slice(0, 3).map(({ mbti, option, ratio }, index) => (
+          <div
+            key={mbti}
+            className={cn("mb-3", { "font-medium": mbti === answer.user.MBTI })}
+          >
             <Icon src={RANK_ICONS[index]} alt={`${index + 1}등`} />
             {mbti}{" "}
             <span className="text-grayscale-60">
