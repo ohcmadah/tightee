76569d2795cb0a82497afc3766b23e2d92cae2ec
Hadam Cho <52340070+ohcmadah@users.noreply.github.com>
2022-12-21 18:27:08 +0900

    chore: fallback 이미지 추가 (#62)

--- src/components/Img.tsx
+++ src/components/Img.tsx
@@ -1,13 +1,4 @@
-import cn from "classnames";
 import { useIsElementInViewport } from "../hooks/useIsElementInViewport";
-
-export type ImgProps = {
-  src: string;
-  alt: string;
-  width: number | "100%";
-  height: number | "100%";
-  lazy?: boolean;
-};
 
 export default function Img(
   props: React.ImgHTMLAttributes<HTMLImageElement> & { lazy?: boolean }
@@ -16,16 +7,12 @@ export default function Img(
     rootMargin: "0px 0px 500px 0px",
   });
   const isLoaded = !props.lazy || isVisible;
-  const fallback = `bg-grayscale-20 w-[${props.width || 20}] h-[${
-    props.height || 20
-  }]`;
 
   return (
     <img
       {...props}
-      className={cn(props.className, { [fallback]: !isLoaded })}
       ref={elementRef}
-      src={isLoaded ? props.src : ""}
+      src={isLoaded ? props.src : "/images/fallback"}
     />
   );
 }
