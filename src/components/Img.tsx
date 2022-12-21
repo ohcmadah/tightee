6d740942068b4cb47f545d977d5d4e978d2a56ec
Hadam Cho <52340070+ohcmadah@users.noreply.github.com>
2022-12-21 19:58:33 +0900

    chore: props에서 lazy 제거하여 넘김 (#63)

--- src/components/Img.tsx
+++ src/components/Img.tsx
@@ -3,14 +3,15 @@ import { useIsElementInViewport } from "../hooks/useIsElementInViewport";
 export default function Img(
   props: React.ImgHTMLAttributes<HTMLImageElement> & { lazy?: boolean }
 ) {
+  const { lazy, ...imgProps } = props;
   const { elementRef, isVisible } = useIsElementInViewport({
     rootMargin: "0px 0px 500px 0px",
   });
-  const isLoaded = !props.lazy || isVisible;
+  const isLoaded = !lazy || isVisible;
 
   return (
     <img
-      {...props}
+      {...imgProps}
       ref={elementRef}
       src={isLoaded ? props.src : "/images/fallback"}
     />
