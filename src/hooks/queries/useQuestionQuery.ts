898156ee69ee15728ca7ba5dff5d4969313ed821
Hadam Cho <chohadam49@gmail.com>
2023-04-06 22:48:11 +0900

    refactor(Report): API 호출 코드 정리

--- src/hooks/queries/useQuestionQuery.ts
+++ src/hooks/queries/useQuestionQuery.ts
@@ -1,25 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
-import { getQuestion } from "../../common/apis";
 import { useQuestionsQuery } from "./useQuestionsQuery";
 
-export const useQuestionQuery = (id: string) => {
+export const useQuestionQuery = (id?: string) => {
   const questions = useQuestionsQuery();
 
-  if (questions.data) {
-    return {
-      ...questions,
-      data: questions.data.find((question) => question.id === id),
-    };
-  }
-
-  return useQuery({
-    queryKey: ["questions", id],
-    queryFn: () => getQuestion(id),
-    select: (res) => {
-      if (res.status === 204) {
-        return null;
-      }
-      return res.data;
-    },
-  });
+  const question = questions.data?.find((question) => question.id === id);
+  return {
+    ...questions,
+    data: question,
+  };
 };
